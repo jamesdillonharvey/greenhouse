a8d27063767a4670d6d2ac808cb151530223e8c7
James Harvey <jamesdillonharvey@users.noreply.github.com>
2020-12-10 09:39:33 +0000

    Update run.sh

--- temp/ds18b20/run.sh
+++ temp/ds18b20/run.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
  
+while true; do 
 # Read Temperature
 tempread=`cat /sys/bus/w1/devices/10-000802b4ba0e/w1_slave`
 # Format
@@ -7,3 +8,5 @@ temp=`echo "scale=2; "\`echo ${tempread##*=}\`" / 1000" | bc`
  
 # Output
 echo "The measured temperature is " $temp "°C"
+sleep 5
+done
