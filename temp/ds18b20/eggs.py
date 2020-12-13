25fced5adbb3fbe510070ad609090d99fd3478ea
James <jamesdillonharvey@gmail.com>
2020-12-13 16:05:37 +0000

    eggs

--- temp/ds18b20/eggs.py
+++ temp/ds18b20/eggs.py
@@ -1,5 +1,3 @@
-
-
 from w1thermsensor import W1ThermSensor
 import RPi.GPIO as GPIO
 import time
@@ -7,13 +5,14 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5, GPIO.OUT)
 
-set_temp = 75;
+set_temp = 30;
 
 try:
-    while true:
+    print("1")
+    while True:
         sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "01191c92e270")
         temp = sensor.get_temperature()
-        print("water temp %.2c target temp %.2c" % (temp, set_temp))
+        print("water temp {0} target temp {1}".format(temp,set_temp))
 
         if temp < set_temp:
             GPIO.output(5, GPIO.LOW)
@@ -22,10 +21,19 @@ try:
             GPIO.output(5, GPIO.HIGH)
             print("OFF")
 
-
         if temp >= set_temp:
-        set_temp = 65
-        print("PHASE 2")
-except:
+            set_temp = 25
+            print("PHASE 2")
+
+        time.sleep(5)
+
+except Exception as e:
+    print(e)    
     GPIO.output(5, GPIO.HIGH)
     print("TURN OFF ERROR")
+
+finally:
+    print("clean up")
+    GPIO.output(5, GPIO.HIGH)
+    GPIO.cleanup() # cleanup all GPIO
+    
